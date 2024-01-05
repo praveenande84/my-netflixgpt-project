83a8b23ed36c37a837ad84cf8d18939f214581a6
andepraveenofficial <praveenande2023@gmail.com>
2024-01-05 17:22:04 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* -----> Third Party Packages <----- */
+import React from 'react';
+
+/* -----> Components <----- */
+const App = () => {
+	console.log('App');
+
+	// Return JSX
+	return <div>App</div>;
+};
+
+/* -----> Export <----- */
+export default App;
